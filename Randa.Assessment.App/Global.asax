9c28dd435a17994571c6a07c4de148eac47ddc0c
zunath <coolty3001@yahoo.com>
2016-07-28 19:48:14 -0500

    Set up angular 2 and typescript

--- Randa.Assessment.App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Randa.Assessment.App.Global" Language="C#" %>
---
+++ Randa.Assessment.App/gulpfile.js
@@ -0,0 +1,60 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var destPath = './wwwroot/libs/';
+
+var paths = {
+    scripts: ['scripts/*.ts',
+              'scripts/*.js',
+              'scripts/**/*.js',
+              'scripts/**/*.ts',
+              'scripts/**/*.map']
+};
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+
+gulp.task("scripts", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap.*js',
+    ], {
+        cwd: "libs/**"
+    })
+        .pipe(gulp.dest("./wwwroot/libs"));
+
+    gulp.src(["Index.html", "systemjs.config.js"])
+        .pipe(gulp.dest("./wwwroot/"))
+});
+
+
+var tsProject = ts.createProject('tsconfig.json');
+gulp.task('ts', function (done) {
+    var tsResult = gulp.src([
+            "scripts/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./wwwroot/app'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch(config.src, ['scripts']);
+});
+
+gulp.task('default', ['ts', 'scripts']);
