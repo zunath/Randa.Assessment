9268e20cbfca881cd4198f82ea499583e7feb35d
zunath <coolty3001@yahoo.com>
2016-07-31 17:37:49 -0500

    Institutions controller

---
+++ Randa.Assessment.Application/Controllers/InstitutionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Randa.Assessment.Domain.Services.Contracts;
+using Randa.Assessment.Domain.Services.Query;
+using Randa.Assessment.Domain.Services.Query.Results;
+
+namespace Randa.Assessment.Application.Controllers
+{
+    public class InstitutionsController : Controller
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        public InstitutionsController(IQueryDispatcher queryDispatcher)
+        {
+            _queryDispatcher = queryDispatcher;
+        }
+
+        // GET: Institutions
+        public ActionResult Index()
+        {
+            GetActiveDistrictsQuery query = new GetActiveDistrictsQuery();
+
+            DistrictListQueryResult result =
+                _queryDispatcher.Execute<GetActiveDistrictsQuery, DistrictListQueryResult>(query);
+
+            return View(result);
+        }
+    }
+}
--- Randa.Assessment.Infrastructure/Repository/DistrictRepository.cs
+++ Randa.Assessment.Infrastructure/Repository/DistrictRepository.cs
@@ -1,13 +1,28 @@
-﻿using Randa.Assessment.Domain.Services.Query.Results;
+﻿using Randa.Assessment.Domain.Contracts;
+using Randa.Assessment.Domain.Entities;
+using Randa.Assessment.Domain.Services.Query.Results;
 using Randa.Assessment.Domain.Services.Repository;
 
 namespace Randa.Assessment.Infrastructure.Repository
 {
     public class DistrictRepository: IDistrictRepository
     {
+        private IValidatorFactory _factory;
+        public DistrictRepository(IValidatorFactory factory)
+        {
+            _factory = factory;
+        }
+
         public DistrictListQueryResult GetActiveDistricts()
         {
-            throw new System.NotImplementedException();
+            DistrictListQueryResult result = new DistrictListQueryResult();
+            result.Add(new District(0, _factory) { Name = "district 1", DistrictId = 1 });
+            result.Add(new District(0, _factory) { Name = "district 2", DistrictId = 2 });
+            result.Add(new District(0, _factory) { Name = "district 3", DistrictId = 3 });
+            result.Add(new District(0, _factory) { Name = "district 4", DistrictId = 4 });
+            result.Add(new District(0, _factory) { Name = "district 5", DistrictId = 5});
+
+            return result;
         }
     }
 }
